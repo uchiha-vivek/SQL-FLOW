6390ad5171500159d6636b3e2cbc6284d4317997
uchiha-vivek <vs160125@gmail.com>
2025-04-02 12:22:07 +0530

    CONCEPTS OF SQL

--- sample.sql
+++ sample.sql
@@ -57,8 +57,52 @@
 -- Delete the entire table from schema
 -- DROP TABLE customer
 
+-- use test_db;
+-- SELECT * FROM  customer;
+
+-- SELECT Name FROM customer;
+
+-- -- finding unique values in a column
+-- SELECT DISTINCT City FROM customer ;
 
 
 
+TRUNCATE TABLE customer;
+SELECT * FROM customer;
+ 
+
+INSERT INTO customer 
+ (ID, Name, Age, City, Salary)
+ VALUES
+ (1, 'Vivek', 22, 'Jamshedpur', 100000),
+ (2, 'Rohit', 21, 'Bokaro', 900000),
+ (3, 'Naman', 21, 'Ranchi', 2000000),
+ (4, 'Prit', 22, 'Jamshedpur', 5000000),
+ (5, 'Amit', 23, 'Patna', 1200000),
+ (6, 'Ravi', 24, 'Dhanbad', 800000),
+ (7, 'Sumit', 22, 'Delhi', 1500000),
+ (8, 'Suresh', 25, 'Kolkata', 2200000),
+ (9, 'Ankit', 23, 'Mumbai', 3500000),
+ (10, 'Raj', 26, 'Bangalore', 4200000),
+ (11, 'Manoj', 28, 'Pune', 600000),
+ (12, 'Deepak', 30, 'Chennai', 2800000),
+ (13, 'Ramesh', 27, 'Hyderabad', 500000),
+ (14, 'Vikas', 29, 'Ahmedabad', 700000),
+ (15, 'Akash', 24, 'Lucknow', 1300000),
+ (16, 'Shyam', 23, 'Noida', 1800000),
+ (17, 'Gopal', 25, 'Jaipur', 2500000),
+ (18, 'Sachin', 26, 'Indore', 3000000),
+ (19, 'Neeraj', 27, 'Bhopal', 2700000),
+ (20, 'Kunal', 28, 'Chandigarh', 3100000);
+
+ 
+--  SELECT Name FROM customer
+--  WHERE Age=22 AND  City='Jamshedpur';
 
 
+-- SELECT * FROM customer
+-- LIMIT 5;
+
+SELECT * FROM customer
+ORDER BY Name ASC
+LIMIT 5;
